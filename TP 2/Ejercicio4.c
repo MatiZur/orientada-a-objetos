9a1f223adc97adad918f0a7db0044a60c1917734
MatiZur <166444039+MatiZur@users.noreply.github.com>
2025-04-01 17:09:14 -0300

    Ejercicio 4 omg

--- TP 2/Ejercicio4.c
+++ TP 2/Ejercicio4.c
@@ -1,18 +1,26 @@
 #include <stdio.h>
 #include <string.h>
-int i;
-char cadena[50], letra[1], reemplazo[2];
+int i, cambio=0;
+char cadena[50], letra, reemplazo;
 
 main(){
 	printf("Ingrese una cadena: ");
 	scanf("%s",cadena);
 	printf("Ingrese una letra a reemplazar: ");
-	scanf("%s",letra);
+	scanf(" %c",&letra);
 	printf("Ingrese la letra de reemplazo: ");
-	scanf("%s",reemplazo);
+	scanf(" %c",&reemplazo);
 	for(i=0 ; i<strlen(cadena) ; i++){
-		if(cadena[i]==letra[0]){
-			memset(cadena, reemplazo[0], cadena[i]);
+		if(cadena[i]==letra){
+			cadena[i]=reemplazo;
+			cambio=1;
 		}
+	}
+	printf("\n");
+	if(cambio==1){
+		printf("La nueva cadena es: %s",cadena);
+	}
+	else{
+		printf("No se encontro la letra a reemplazar");
 	}
 }
