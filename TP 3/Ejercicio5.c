a5f7d6937bfc1f04cfe8dd3f1c1648b2b59c6344
MatiZur <166444039+MatiZur@users.noreply.github.com>
2025-04-22 17:06:47 -0300

    ejer 5

---
+++ TP 3/Ejercicio5.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+void concatenar(char *cadena1, char *cadena2){
+	strcat(cadena1, cadena2);
+}
+
+int main(){
+	char cadena1[99], cadena2[99];
+	printf("Ingrese la cadena 1: ");
+	scanf("%s",cadena1);
+	printf("Ingrese la cadena 2: ");
+	scanf("%s",cadena2);
+	concatenar(cadena1, cadena2);
+	printf("\nLas cadenas concatenadas quedan: %s",cadena1);
+	return 0;
+}
