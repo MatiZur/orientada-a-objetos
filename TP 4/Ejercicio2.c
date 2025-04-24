f0f793b29085df9f849198884a731275812e7a68
MatiZur <166444039+MatiZur@users.noreply.github.com>
2025-04-23 21:42:14 -0300

    exercise dos
    lo tenia guardado como 1 jaja

--- TP 4/Ejercicio2.c
+++ TP 4/Ejercicio2.c
@@ -1,76 +1,89 @@
 #include <stdio.h>
+#include <string.h>
+FILE *archivo;
+
+void carga(){
+     int cant, i, dni;
+     char nombre[99], apellido[99];
+     archivo = fopen("datos.txt", "w");
+     printf("\nIngrese la cantidad de personas a registrar: ");
+     scanf("%d", &cant);
+     for( i = 0 ; i < cant ; i++ ){
+          printf("\nIngrese el nombre: ");
+          scanf("%s", nombre);
+          printf("Ingrese el apellido: ");
+          scanf("%s", apellido);
+          printf("Ingrese el DNI: ");
+          scanf("%d", &dni);
+          fprintf(archivo, "%s %s %d\n", nombre, apellido, dni);
+     }
+     fclose(archivo);
+     printf("\nDatos cargados exitosamente.\n\n\n");
+}
+
+void lectura(){
+     archivo = fopen("datos.txt", "r");
+     if(archivo == NULL){
+          printf("Error al abrir el archivo para lectura.\n");
+          return;
+     }
+     int opcion, dni, encontrado=0;
+     char nombre[99], apellido[99];
+     printf("\n1: Buscar por DNI | 2: Buscar por nombre y apellido\nIngrese su opcion: ");
+     scanf("%d", &opcion);
+     switch(opcion){
+          case 1:{
+               int dniBuscar;
+               printf("\nIngrese el DNI de la persona que desea buscar: ");
+               scanf("%d", &dniBuscar);
+               while( fscanf(archivo, "%s %s %d", nombre, apellido, &dni) != EOF ){
+                    if(dni == dniBuscar){
+                         printf("\nEl DNI %d pertenece a %s, %s.\n\n\n", dniBuscar, apellido, nombre);
+                         encontrado = 1;
+                         break;
+                    }
+               }
+               break;
+          }
+          case 2:{
+               char nombreBuscar[99], apellidoBuscar[99];
+               printf("\nIngrese el nombre y el apellido de la persona que desea buscar: ");
+               scanf("%s %s", nombreBuscar, apellidoBuscar);
+               while( fscanf(archivo, "%s %s %d", nombre, apellido, &dni) != EOF ){
+                    if(strcmp(nombre, nombreBuscar) == 0 && strcmp(apellido, apellidoBuscar) == 0){
+                         printf("\n%s, %s tiene el DNI %d.\n\n\n", apellidoBuscar, nombreBuscar, dni);
+                         encontrado = 1;
+                         break;
+                    }
+               }
+               break;
+          }
+     }
+     if(encontrado == 0){
+          printf("\nLa persona no fue encontrada.\n\n\n");
+     }
+     fclose(archivo);
+}
 
 int main(int argc, char** argv){
-	FILE *archivo;
-	char nombre[99], apellido[99];
-	int opcion, i, cant, dni;
-	do{
-		printf("1: Cargar datos | 2: Leer datos | 3: Terminar programa\nIngrese su opcion: ");
-		scanf("%d", &opcion);
-		switch(opcion){
-			case 1:
-				archivo = fopen("datos.txt", "w");
-				printf("\nIngrese la cantidad de personas a registrar: ");
-				scanf("%d", &cant);
-				for( i = 0 ; i < cant ; i++ ){
-					printf("\nIngrese el nombre: ");
-					scanf("%s", nombre);
-					printf("Ingrese el apellido: ");
-					scanf("%s", apellido);
-					printf("Ingrese el DNI: ");
-					scanf("%d", &dni);
-					fprintf(archivo, "%s %s %d\n", nombre, apellido, dni);
-				}
-				fclose(archivo);
-				printf("\nDatos cargados exitosamente.\n\n\n");
-				break;
-			case 2:
-				int opcionR, encontrado=0;
-				printf("1: Buscar por DNI | 2: Buscar por nombre y apellido\nIngrese su opcion: ");
-				scanf("%d", &opcionR);
-				archivo = fopen("datos.txt", "r");
-				if(archivo == NULL){
-					printf("Error al abrir el archivo para lectura.\n");
-					return 0;
-				}
-				switch(opcionR){
-					case 1:
-						int dniBuscar;
-					    printf("\nIngrese el DNI de la persona que desea buscar: ");
-					    scanf("%d", &dniBuscar);
-					    while( fscanf(archivo, "%s %s %d", nombre, apellido, &dni) != EOF ){
-					        if(dni == dniBuscar){
-					            printf("\nEl DNI %d pertenece a %s, %s.\n\n\n", dniBuscar, apellido, nombre);
-					            encontrado = 1;
-					            break;
-					        }
-					    }
-					    break;
-					case 2:
-						int nombreBuscar, apellidoBuscar;
-						printf("\nIngrese el nombre y el apellido de la persona que desea buscar: ");
-						scanf("%s %s", nombreBuscar, apellidoBuscar);
-						while( fscanf(archivo, "%s %s %d", nombre, apellido, &dni) != EOF ){
-					        if((nombre == nombreBuscar) && (apellido == apellidoBuscar)){
-					            printf("\n%s, %s tiene el DNI %d.\n\n\n", apellidoBuscar, nombreBuscar, dni);
-					            encontrado = 1;
-					            break;
-					        }
-					    }
-					    break;
-				}
-				if(encontrado == 0){
-					printf("\nLa persona no fue encontrada.\n\n\n");
-				}
-				fclose(archivo);
-				break;
-			case 3:
-				printf("\nTerminando programa...");
-				break;
-			default:
-				printf("\nOpcion no reconocida.\n\n\n");
-				break;
-		}
-	}while(opcion != 3);
-	return 0;
+     int opcion;
+     do{
+          printf("1: Cargar datos | 2: Leer datos | 3: Terminar programa\nIngrese su opcion: ");
+          scanf("%d", &opcion);
+          switch(opcion){
+                 case 1:
+                      carga();
+                      break;
+                 case 2:
+                      lectura();
+                      break;
+                 case 3:
+                      printf("\nTerminando programa...");
+                      break;
+                 default:
+                      printf("\nOpcion no reconocida.\n\n\n");
+                      break;
+          }
+     }while(opcion != 3);
+     return 0;
 }
