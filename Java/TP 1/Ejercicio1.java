080512f30de9b945899b7211695acfc5c5de018d
MatiZur <166444039+MatiZur@users.noreply.github.com>
2025-06-05 09:30:33 -0300

    ejer1

--- Java/TP 1/Ejercicio1.java
+++ Java/TP 1/Ejercicio1.java
@@ -1,10 +1,10 @@
 public class Ejercicio1{
 	public static void main(String[] args){
-		for(int i = 1 ; i < 101 ; i++){			// Iniciar ciclo de 1 hasta 100
-			if(i % 2 != 0){				// Si la resta de dividir i por 2 no da 0 (si es impar)
+		for(int i = 1 ; i < 101 ; i++){		// Iniciar ciclo de 100 iteraciones empezando desde 1
+			if(i % 2 != 0){					// Si la resta de dividir i por 2 no da 0 (si es impar)
 				System.out.println(i);		// Imprimir i
 			}
-			else{					// Si no ocurre lo anterior (la resta de i divido 2 da 0, es par)
+			else{							// Si no ocurre lo anterior (la resta de i divido 2 da 0, es par)
 				System.out.println("Par");	// Imprimir "Par"
 			}
 		}
