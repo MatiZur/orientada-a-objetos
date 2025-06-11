d7d671329c9048ba2a334bbfb83914b7b4b94827
MatiZur <166444039+MatiZur@users.noreply.github.com>
2025-06-11 16:24:36 -0300

    arreglados comentarios

--- Java/TP 1/Ejercicio8.java
+++ Java/TP 1/Ejercicio8.java
@@ -8,19 +8,19 @@ public class Ejercicio8{
 		int numero, intentos = 0, adivinar = random.nextInt(100) + 1;											// Iniciar variable de intentos y generar un nero aleatorio entre 0 y 99 y sumarle 1 para que cumpla con la consigna (entre 1 a 100 inclusive)
 		System.out.println("¡Bienvenido a Adivina el Número! Se va a generar un número del 1 al 100 y tu objetivo es adivinarlo.");
 		do{
-			System.out.print("\nIngrese un nero: ");
+			System.out.print("\nIngrese un número: ");
 			numero = scanner.nextInt();
 			intentos++;																							// Sumar un intento
-			if(numero < adivinar){																				// Si el nero ingresado es menor
-				System.out.print("El nero ingresado es menor al generado.\n");								// Dar pista
+			if(numero < adivinar){																				// Si el número ingresado es menor
+				System.out.print("El número ingresado es menor al generado.\n");								// Dar pista
 			}
-			else if(numero == adivinar){																		// Si el nero es el generado
-				System.out.print("\n；elicidades! Acertaste el nero en " + intentos + " intento/s.");			// Mensaje de felicitaciones
+			else if(numero == adivinar){																		// Si el número es el generado
+				System.out.print("\n¡Felicidades! Acertaste el número en " + intentos + " intento/s.");			// Mensaje de felicitaciones
 			}
-			else if(numero > adivinar){																			// Si el nero ingresado es mayor
-				System.out.print("El nero ingresado es mayor al generado.\n");								// Dar pista
+			else if(numero > adivinar){																			// Si el número ingresado es mayor
+				System.out.print("El número ingresado es mayor al generado.\n");								// Dar pista
 			}
-		}while(numero != adivinar);																				// Repetir hasta adivinar el nero
+		}while(numero != adivinar);																				// Repetir hasta adivinar el número
 		scanner.close();
 	}
 }
