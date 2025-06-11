4f43f9c1898bdbfd10e70a161636892cc9c4ced9
MatiZur <166444039+MatiZur@users.noreply.github.com>
2025-06-11 08:30:16 -0300

    added comentarios

--- Java/TP 1/Ejercicio5.java
+++ Java/TP 1/Ejercicio5.java
@@ -7,14 +7,14 @@ public class Ejercicio5{
 		do{
 			System.out.print("Ingrese un número del 1 al 10: ");
 			num = scanner.nextInt();
-			if(num < 1 || num > 10){
-				System.out.print("\nEl número ingresado no es del 1 al 10.\n\n\n");
+			if(num < 1 || num > 10){													// Validación de datos
+				System.out.print("\nEl número ingresado no es del 1 al 10.\n\n\n");		// Informar que el número ingresado no es correcto
 			}
-		}while(num < 1 || num > 10);
+		}while(num < 1 || num > 10);													// No salir si el número no está entre 1 y 10
 		scanner.close();
-		System.out.print("\nLa tabla de multiplicar de " + num + " es: " + num + " ");
-		for(int i = 2 ; i < 11 ; i++){
-			System.out.print(num * i + " ");
+		System.out.print("\nLa tabla de multiplicar de " + num + " es: ");
+		for(int i = 1 ; i <= 10 ; i++){													// Ciclo desde 1 hasta 10
+			System.out.print(num * i + " ");											// Imprimir el número multiplicado por i
 		}
 	}
 }
