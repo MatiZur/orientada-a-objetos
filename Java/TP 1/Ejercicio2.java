f49332aaec1bda6ca51b090b0e879a02c8525eb8
MatiZur <166444039+MatiZur@users.noreply.github.com>
2025-06-11 08:09:09 -0300

    arreglado un comentario

--- Java/TP 1/Ejercicio2.java
+++ Java/TP 1/Ejercicio2.java
@@ -2,9 +2,9 @@ public class Ejercicio2{
 	public static void main(String[] args){
 		boolean noPrimo = false;					// Booleano para saber si i es no primo
 		for(int i = 50 ; i < 101 ; i++){			// Iniciar ciclo de 50 iteraciones empezando desde 50 (de 50 hasta 100)
-			for(int j = 2 ; j < (i / 2 + 1) ; j++){	// Iniciar ciclo desde 2 (1 siempre va a ser divisor) hasta la mitad del número más 1 (para no hacer calculos innecesarios, ya que después de la mitad no va a haber ningun divisor)
-				if(i % j == 0){						// Si la resta de dividir el i por j da 0 (entonces j es divisor de i)
-					noPrimo = true;					// Cambiamos booleano a verdadero (para hacer el salto de linea más tarde)
+			for(int j = 2 ; j < (i / 2 + 1) ; j++){	// Iniciar ciclo desde 2 (1 siempre va a ser divisor) hasta la mitad del nÃºmero mÃ¡s 1 (para no hacer calculos innecesarios, ya que despuÃ©s de la mitad no va a haber ningun divisor)
+				if(i % j == 0){						// Si el resto de la divisiÃ³n de i por j da 0 (entonces j es divisor de i)
+					noPrimo = true;					// Cambiamos booleano a verdadero (para hacer el salto de linea mÃ¡s tarde)
 					System.out.print(j + " ");		// Imprimir divisor
 				}
 			}
