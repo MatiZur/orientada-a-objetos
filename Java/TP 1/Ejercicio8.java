55c05538faba26abd50326257e65a9869d642e1b
MatiZur <166444039+MatiZur@users.noreply.github.com>
2025-06-11 16:23:43 -0300

    Update Ejercicio8.java

--- Java/TP 1/Ejercicio8.java
+++ Java/TP 1/Ejercicio8.java
@@ -5,22 +5,22 @@ public class Ejercicio8{
 	public static void main(String[] args){
 		Scanner scanner = new Scanner(System.in);
 		Random random = new Random();																			// Declarar random
-		int numero, intentos = 0, adivinar = random.nextInt(100) + 1;											// Iniciar variable de intentos y generar un número aleatorio entre 0 y 99 y sumarle 1 para que cumpla con la consigna (entre 1 a 100 inclusive)
-		System.out.println("¡Bienvenido a Adivina el Número! Se va a generar un número del 1 al 100 y tu objetivo es adivinarlo.");
+		int numero, intentos = 0, adivinar = random.nextInt(100) + 1;											// Iniciar variable de intentos y generar un nî€­ero aleatorio entre 0 y 99 y sumarle 1 para que cumpla con la consigna (entre 1 a 100 inclusive)
+		System.out.println("Â¡Bienvenido a Adivina el NÃºmero! Se va a generar un nÃºmero del 1 al 100 y tu objetivo es adivinarlo.");
 		do{
-			System.out.print("\nIngrese un número: ");
+			System.out.print("\nIngrese un nî€­ero: ");
 			numero = scanner.nextInt();
 			intentos++;																							// Sumar un intento
-			if(numero < adivinar){																				// Si el número ingresado es menor
-				System.out.print("El número ingresado es menor al generado.\n");								// Dar pista
+			if(numero < adivinar){																				// Si el nî€­ero ingresado es menor
+				System.out.print("El nî€­ero ingresado es menor al generado.\n");								// Dar pista
 			}
-			else if(numero == adivinar){																		// Si el número es el generado
-				System.out.print("\n¡Felicidades! Acertaste el número en " + intentos + " intento/s.");			// Mensaje de felicitaciones
+			else if(numero == adivinar){																		// Si el nî€­ero es el generado
+				System.out.print("\nï¼›elicidades! Acertaste el nî€­ero en " + intentos + " intento/s.");			// Mensaje de felicitaciones
 			}
-			else if(numero > adivinar){																			// Si el número ingresado es mayor
-				System.out.print("El número ingresado es mayor al generado.\n");								// Dar pista
+			else if(numero > adivinar){																			// Si el nî€­ero ingresado es mayor
+				System.out.print("El nî€­ero ingresado es mayor al generado.\n");								// Dar pista
 			}
-		}while(numero != adivinar);																				// Repetir hasta adivinar el número
+		}while(numero != adivinar);																				// Repetir hasta adivinar el nî€­ero
 		scanner.close();
 	}
 }
