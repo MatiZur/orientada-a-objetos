e5249305f3d10529176a62118eb952f4ce91c9ba
MatiZur <166444039+MatiZur@users.noreply.github.com>
2025-06-17 17:24:56 -0300

    ejercicio 20

--- Java/TP 1/Ejercicio20.java
+++ Java/TP 1/Ejercicio20.java
@@ -8,9 +8,9 @@ public class Ejercicio20{
 		System.out.print("Ingrese la longitud de la contraseña deseada: ");
 		int tipo, longitud = scanner.nextInt();
 		scanner.close();
-		String minus = "abcdefghijklmnopqrstuvwxyz";						// String para guardar todas las minusculas
-		String mayus = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";						// String para guardar todas las minusculas
-		System.out.print("\n");
+		String minus = "abcdefghijklmnopqrstuvwxyz";						// String para guardar todas las minúsculas
+		String mayus = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";						// String para guardar todas las mayúsculas
+		System.out.print("\nContraseña generada: ");
 		for(int i = 0 ; i < longitud ; i++){
 			tipo = random.nextInt(3);										// Definimos el tipo de carácter aleatoriamente
 			if(tipo == 0){													// Si el tipo es 0 va a ser un número
